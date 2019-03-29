fd46de4adeb6512eacfe49902f705a57b068be6b
张宽顺 <746822492@qq.com>
2019-03-29 16:41:02 +0800

    axios,本地跨域

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,18 +1,32 @@
 <template>
-    <div>这是登录页</div>
+    <div>
+        <el-input v-model="vin"></el-input>
+        <el-button size="mini" @click="getInfo">确定</el-button>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
+            vin: ''
+        }
+    },
+    methods: {
+        getInfo() {
+            const param = {
+                vin: this.vin,
+                currentPage: 1,
+                pageSize: 10,
+            }
+            this.$axios.get('/da', param).then((res) => {
 
+            }).catch(() => {
+                this.$message.error('接口报错')
+            })
         }
     },
     mounted() {
-        setTimeout(() => {
-            this.$router.push('/index')
-        }, 2000)
     }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,25 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+
 Vue.use(ElementUI)
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://localhost:8080/api';
+Vue.prototype.$axios = axios;
+axios.interceptors.request.use(config => {
+    console.log(config)
+	return config;
+}, error => {
+	return Promise.reject(error);
+});
+axios.interceptors.response.use(response => {
+    console.log(response)
+	return response;
+}, error => {
+	return Promise.reject(error);
+});
 
 /* eslint-disable no-new */
 new Vue({
