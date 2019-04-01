1d058aa921b03ba1e306e3a5f37daac8b76fec64
张宽顺 <746822492@qq.com>
2019-04-01 11:34:03 +0800

    新导入样式文件

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import './styles/common.less'
 import axios from 'axios'
 
 Vue.use(ElementUI)
